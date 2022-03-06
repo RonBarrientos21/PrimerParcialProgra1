df5487f08e3d169305737b135e707765e48a103f
RonBarrientos21 <rbarrientosa1@miumg.edu.gt>
2022-03-06 16:00:06 -0600

    Se agrega la funcion factorial para examen parcial numero 1 en repositorio remoto github

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    int i, //variable local//
+    producto = 1;
+
+    for (i = n; i > 1; --i) {
+        producto *= i;
+    }
+
+    return producto;
+}
+
+int main(void)
+{
+    int num, fact;
+    printf("Ingrese un entero igual o mayor que cero: ");
+    scanf("%d", &num);
+    if (num < 0) {
+        printf("El factorial de un numero negativo (%d) es indefinido\n", num);
+    }
+    else if (num <= 30) {
+        fact = factorial(num);
+        printf("El factorial de %d es %d\n", num, fact);
+
+    }
+
+    return (0);
+
+
+}
